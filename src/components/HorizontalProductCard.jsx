2cb1ad9a6ce112e27be99eceaa3692cdac2b05f3
ajithkumar2498 <ajibhc24@gmail.com>
2024-11-13 23:44:46 +0530

    sds

--- src/components/HorizontalProductCard.jsx
+++ src/components/HorizontalProductCard.jsx
@@ -56,7 +56,7 @@ const HorizontalProductCard = ({
                                loadingList.map((product, index)=>{
                             
                                 return (
-                                    <div className = 'w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] h-36 bg-white rounded-sm shadow flex'>
+                                    <div key={index} className = 'w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] h-36 bg-white rounded-sm shadow flex'>
                                         <div className='bg-slate-100 h-full p-4 min-w-[120px] md:min-w-[145px] animate-pulse'>
                                             
                                         </div>
--- src/components/VerticalCardProduct.jsx
+++ src/components/VerticalCardProduct.jsx
@@ -52,7 +52,7 @@ const VerticalProductCard = ({
                             loadingList.map((product, index)=>{
                             
                                 return (
-                                    <div className = 'w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow'>
+                                    <div key={index} className = 'w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow'>
                                         <div className='bg-slate-200 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center animate-pulse '>
                             
                                         </div>
@@ -72,7 +72,7 @@ const VerticalProductCard = ({
                             data.map((product, index)=>{
                             
                                 return (
-                                    <Link to={`product/${product._id}`} className = 'w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow'>
+                                    <Link to={`product/${product._id}`} key={product._id} className = 'w-full min-w-[280px] md:min-w-[320px] max-w-[280px] md:max-w-[320px] bg-white rounded-sm shadow'>
                                         <div className='bg-slate-100 h-48 p-4 min-w-[280px] md:min-w-[145px] flex justify-center items-center'>
                                              <img src={product.productImage[0]} className='object-scale-down mix-blend-multiply cursor-pointer h-full hover:scale-110 transition-all'/>
                                         </div>
